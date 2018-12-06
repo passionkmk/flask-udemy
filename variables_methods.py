e03767813590ae09225b9e8663e661340cb58888
pkmk <passion.kmk@gmail.com>
2018-12-06 13:46:08 +0900

    Create variable_methods.py

---
+++ variables_methods.py
@@ -0,0 +1,29 @@
+a = 5
+b = 10
+my_variable = 56
+my_10_variable = 10
+
+string_variable = "hello"
+single_quote = 'strings can have single quotes'
+
+# print(my_variable)
+# print(string_variable)
+# print("hello, world!")
+# print(123)
+
+## comment
+    # 들여쓰기 -> indented
+# () -> bracket
+
+def my_print_method(my_argument):
+    print(my_argument)
+
+# my_print_method("hello, world!")
+
+def my_multiply_method(number_one, number_two):
+    return number_one * number_two
+
+# 대입 -> assigned
+
+# result = my_multiply_method(5, 3)
+my_print_method(my_multiply_method(5, 3))
