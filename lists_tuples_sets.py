550b58ccb1f55f2fd11da28d1b276e88206574c5
pkmk <passion.kmk@gmail.com>
2018-12-12 13:16:32 +0900

    Create list_tuples_sets.py

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -1,4 +1,10 @@
 my_variable = "hello"
-grades = [77, 80, 90, 95, 100]
+grades = [77, 80, 90]
+tuple_grades = (77, 80, 90) # immutable 불변
+set_grades = {77, 80, 90} # unique & unordered
 
-print(len(grades))
+tuple_grades = tuple_grades + (100,) #튜블로 하려면 (100,) 쉼표가 들어가야함
+set_grades.add(60)
+print(tuple_grades)
+print(set_grades)
+
