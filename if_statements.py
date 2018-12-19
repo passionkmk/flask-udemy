95c99a0d716096117eae04e4f242082e3c0ef1a6
passionkmk <passion.kmk@gmail.com>
2018-12-20 03:17:45 +0900

    Editing if_statement.py about Section2-18

--- if_statements.py
+++ if_statements.py
@@ -1,16 +1,24 @@
 # should_continue = True
 # if should_continue:
 #     print("Hello")
-person = ""
-while person != "exit":
-    knwon_people = ["John", "Anna", "Mary"]
-    person = input("Enter the person you know: ")
-    if person == "exit":
-        print("Correct! You must go out!")
-        break
+# person = ""
+# while person != "exit":
+#     knwon_people = ["John", "Anna", "Mary"]
+#     person = input("Enter the person you know: ")
+#     if person == "exit":
+#         print("Correct! You must go out!")
+#         break
 
-    if person in knwon_people:
-        print("You know this person!")
-    else:
-        print("You don't know this person!")
+#     if person in knwon_people:
+#         print("You know this person!")
+#     else:
+#         print("You don't know this person!")
         
+knwon_people = ["John", "Anna", "Mary"]
+person = input("Enter ")
+
+def who_do_you_know():
+    pass
+
+def ask_user():
+    pass
