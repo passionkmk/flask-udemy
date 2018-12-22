f95cde6d74b0166dec4984ca99786903f21116e9
passionkmk <passion.kmk@gmail.com>
2018-12-22 18:08:00 +0900

    Add Student Example about class

--- classes_object.py
+++ classes_object.py
@@ -15,5 +15,19 @@ player_one = LottteryPlayer("Rolf")
 player_one.name = (1, 2, 3, 6, 7, 8)
 player_two = LottteryPlayer("Jose")
 
-print(player_one.name == player_two.name)
+# print(player_one.name == player_two.name)
 
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def avarage(self):
+        return sum(self.marks) / len(self.marks)
+
+anna = Student("Anna", "MIT")
+anna.marks.append(56)
+anna.marks.append(71)
+print(anna.avarage())
+        
