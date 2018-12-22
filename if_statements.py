fe6a5a1206d811b74c45078fca2c5ddd6e9f27e0
passionkmk <passion.kmk@gmail.com>
2018-12-22 16:37:36 +0900

    Add function for list

--- if_statements.py
+++ if_statements.py
@@ -14,11 +14,20 @@
 #     else:
 #         print("You don't know this person!")
         
-knwon_people = ["John", "Anna", "Mary"]
-person = input("Enter ")
+# knwon_people = ["John", "Anna", "Mary"]
+# person = input("Enter ")
 
 def who_do_you_know():
-    pass
+    people = input("Enter the names of people you know, separated by commas: ")
+    people_list = people.split(",")
+    people_whithout_space =[]
+    for person in people_list:
+        people_whithout_space.append(person.strip())
+    return people_list
 
 def ask_user():
-    pass
+    person = input("Enter a name of someone you know: ")
+    if person in who_do_you_know():
+        print("You know {}!".format(person))
+
+ask_user()
