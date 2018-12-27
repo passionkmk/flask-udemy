b0c2918a8d65053e134e1f5cc3b328d575b21787
pkmk <passion.kmk@gmail.com>
2018-12-27 13:20:22 +0900

    Add classmethod, staticmethod

--- classes_object.py
+++ classes_object.py
@@ -26,8 +26,27 @@ class Student:
     def avarage(self):
         return sum(self.marks) / len(self.marks)
 
+    def go_to_school(self):
+        print("I'm going to {}.".format(self.school))
+
+    @classmethod
+    def must_go_to_school(cls):
+        print("I must go to school. I don't care anymore!")
+
+    @staticmethod
+    def finally_go_to_school():
+        print("Finally. I'm go to school.")
+
 anna = Student("Anna", "MIT")
+rolf = Student("Rolf", "Oxford")
+
 anna.marks.append(56)
 anna.marks.append(71)
-print(anna.avarage())
+# anna.go_to_school()
+anna.must_go_to_school()
+anna.finally_go_to_school()
+Student.finally_go_to_school()
+
+# rolf.go_to_school()
+# rolf.must_go_to_school()
         
