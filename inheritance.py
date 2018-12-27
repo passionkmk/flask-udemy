f1ada560c9a653f184f0f4a23300de19caa96c96
pkmk <passion.kmk@gmail.com>
2018-12-27 13:35:11 +0900

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Student: 
+    def __init__(self. name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+    
+    def friend(self, friend_name)
+        return Student(friend_name, self.school)
