3786078c9cd2d445668b185204ab30f39e6ad8d5
pkmk <passion.kmk@gmail.com>
2019-01-02 13:41:34 +0900

    Add store api modeling

--- rest_api/app.py
+++ rest_api/app.py
@@ -2,8 +2,44 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # https://google.com/
-def home():
-    return "Hello World"
+store = [
+    {
+        'name': 'My Wonderful Store',
+        'item' : [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+# POST - used to recieve data
+# GET - used to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', method=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>') # 'http://127.0.0.1:5000/store/some_name'
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', method=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 app.run(port=5000)
