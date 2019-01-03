c8461cbee322a402223a65799ad15715e02d8cde
pkmk <passion.kmk@gmail.com>
2019-01-03 13:54:18 +0900

    Add templetes index.html

--- rest_api/app.py
+++ rest_api/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+# Flask, jsonify - JSON package, request - Http request package
+# render_template
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -14,18 +16,35 @@ stores = [
     }
 ]
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 # POST - used to recieve data
 # GET - used to send data back only
 
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>') # 'http://127.0.0.1:5000/store/some_name'
 def get_store(name):
-    pass
+    # Iterate over stores
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+    # If the store name matches, return it
+    # If none match, return an error message
 
 # GET /store
 @app.route('/store')
@@ -35,11 +54,23 @@ def get_stores():
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store['item'])
+    return jsonify({'message': 'store not found'})
 
 app.run(port=5000)
