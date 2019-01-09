b21c55bc8a364a984eae180ac53844f1273020ff
pkmk <passion.kmk@gmail.com>
2019-01-09 13:13:56 +0900

    Add restful_app request json

--- restful_app/code/app.py
+++ restful_app/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -14,10 +14,16 @@ class Item(Resource):
         return {'item': None}, 404 # Not found error code
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json() # request가 JSON인지 확인, force=True force를 True로 해두면 application/JSON Header 설정을 하지 않아도 됨 근데 위험
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201 # Created code
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
     
 api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1:5000/student/Rolf
+api.add_resource(ItemList, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
