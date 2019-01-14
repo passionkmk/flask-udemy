e2dce6e12f888a6baf1a41b75bb906c0a96f1444
passionkmk <passion.kmk@gmail.com>
2019-01-14 23:24:04 +0900

    Installed Flask-JWT and Add security, user class for authentication

---
+++ restful_app/code/security.py
@@ -0,0 +1,30 @@
+users =[
+    {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+]
+
+username_mapping = { 'bob': {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
+
+userid_mapping = { 1: {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
+
+def authenticate(username, password): # 패스워드 검사
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
