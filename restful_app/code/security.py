9447795cd29838d6d131f20ed62b3f6c2c0b88f5
passionkmk <passion.kmk@gmail.com>
2019-01-15 13:17:44 +0900

    Modified app, security

--- restful_app/code/security.py
+++ restful_app/code/security.py
@@ -1,24 +1,31 @@
-users =[
-    {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
+from user import User
+
+# users =[
+#     {
+#         'id': 1,
+#         'username': 'bob',
+#         'password': 'asdf'
+#     }
+# ]
+users = [
+    User(1, 'bob', 'asdf')
 ]
 
-username_mapping = { 'bob': {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
+# username_mapping = { 'bob': {
+#         'id': 1,
+#         'username': 'bob',
+#         'password': 'asdf'
+#     }
+# }
+username_mapping = {u.username: u for u in users}
 
-userid_mapping = { 1: {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
+# userid_mapping = { 1: {
+#         'id': 1,
+#         'username': 'bob',
+#         'password': 'asdf'
+#     }
+# }
+userid_mapping = {u.user_id: u for u in users}
 
 def authenticate(username, password): # 패스워드 검사
     user = username_mapping.get(username, None)
